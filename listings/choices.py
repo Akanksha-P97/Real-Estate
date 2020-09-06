ab69430880c3b32ff2cecb96491ecfeb04f1c4b3
Akanksha Pandey <akanksha.pandey@suiit.ac.in>
2020-09-06 15:24:15 +0530

    first commit

---
+++ listings/choices.py
@@ -0,0 +1,37 @@
+bedroom_choices = {
+  '1':1,
+  '2':2,
+  '3':3,
+  '4':4,
+  '5':5,
+  }
+
+price_choices = {
+  '100':'€500',
+  '500':'€1000',
+  '1000':'€1500',
+  '1500':'€2000',
+  '2000':'€2500',
+}
+
+state_choices = {
+        
+       'BW': 'Baden Wurttemberg',
+       'BAV': 'Bavaria' ,
+       'BER': 'Berlin',
+       'BRN':'Brandenburg',
+       'BMN':'Bremen',
+       'HAM':'Hamburg',
+       'HSE':'Hesse',
+       'LS':'Lower Saxony',
+       'MV': 'Mecklenburg Vorpommern',
+       'NRW':'North Rhine Westphalia',
+       'RP': 'Rhineland Palatinate',
+       'SAR': 'Saarland',
+       'SAX': 'Saxony',
+       'SA': 'Saxony-Anhalt',
+       'SWG': 'Schleswig',
+       'SH': 'Schleswig Holstein',
+       'THU': 'Thuringia',
+       
+}
